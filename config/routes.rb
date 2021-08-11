36d96fb61089fc966031ff77c4f705d38b313fea
hebusch <hwbusch@uc.cl>
2021-08-11 15:16:41 -0400

    Instalado Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             },
+             path: '', path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               sign_up: 'register'
+             }
   root 'home#index'
 end
